0fced3532cc3ad6810b0c3d451df430ae45d5a38
Jatin Parekh <jatinparekh93@gmail.com>
2014-05-23 13:09:48 +0530

    Reader for Line String, Multi Line String, Polygon(Without Holes), Multi Polygon added.

--- vtkgeojsonfeature.cpp
+++ vtkgeojsonfeature.cpp
@@ -6,11 +6,18 @@ vtkGeoJSONFeature::vtkGeoJSONFeature()
 
 vtkPolyData *vtkGeoJSONFeature::GetOutput()
 {
+    //return the generated polyData after generating the combined polydata from the geoJSON features
+    //Check if output data contains appropriate data and return values accordingly
     return outputData;
 }
 
 double *vtkGeoJSONFeature::createPoint(Json::Value coordinates)
 {
+    //Obtain the point in double[] format from its corresponding geoJSON format
+    //Check if Coordinates corresponds to Point
+    //Do isDouble before asDouble to prevent inconsistency
+    //Probably check for float/int too
+
     double *point = new double[3];
     if(coordinates.size() == 1)
     {
@@ -35,9 +42,11 @@ double *vtkGeoJSONFeature::createPoint(Json::Value coordinates)
     }
     return point;
 }
-
 vtkPolyData *vtkGeoJSONFeature::extractPoint(Json::Value coordinates)
 {
+    //Extract the point from geoJSON geometry and generate the corresponding vtkPolyData
+    //Check if Coordinates corresponds to Single Point
+
     vtkPolyData *pointPolyData = vtkPolyData::New();
     double *point = createPoint(coordinates);
 
@@ -45,7 +54,7 @@ vtkPolyData *vtkGeoJSONFeature::extractPoint(Json::Value coordinates)
     vtkIdType pid[PID_SIZE];
 
     vtkPoints *points = vtkPoints::New();
-    pid[0] = points->InsertNextPoint(point[0], point[1], point[2]);
+    pid[0] = points->InsertNextPoint(point);
 
     vtkCellArray *verts = vtkCellArray::New();
     verts->InsertNextCell(PID_SIZE, pid);
@@ -55,10 +64,11 @@ vtkPolyData *vtkGeoJSONFeature::extractPoint(Json::Value coordinates)
 
     return pointPolyData;
 }
-
-
 vtkPolyData *vtkGeoJSONFeature::extractMultiPoint(Json::Value coordinates)
 {
+    //Extract multipple points from the geoJSON geometry and create the corresponding vtkPolyData
+    //Check if Coordinates corresponds to Multi Points
+
     vtkPolyData *multiPointPolyData = vtkPolyData::New();
 
     if(coordinates.isArray())
@@ -72,7 +82,7 @@ vtkPolyData *vtkGeoJSONFeature::extractMultiPoint(Json::Value coordinates)
         for(int i = 0; i < PID_SIZE; i++)
         {
             double *point = createPoint(coordinates[i]);
-            pid[i] = points->InsertNextPoint(point[0], point[1], point[2]);
+            pid[i] = points->InsertNextPoint(point);
         }
         verts->InsertNextCell(PID_SIZE, pid);
 
@@ -81,29 +91,126 @@ vtkPolyData *vtkGeoJSONFeature::extractMultiPoint(Json::Value coordinates)
     }
     return multiPointPolyData;
 }
-
 vtkPolyData *vtkGeoJSONFeature::extractLineString(Json::Value coordinates)
 {
-    return vtkPolyData::New();
-}
+    //extract the line string from the geoJSON geometry and create the corresponding vtkPolyData
+    //Check if Coordinates corresponds to Line String
 
+    vtkPolyData *linesPolyData = vtkPolyData::New();
+    vtkCellArray *lines = vtkCellArray::New();
+    vtkPoints *points = vtkPoints::New();
+
+    int LINE_COUNT = coordinates.size();
+
+    double *start = createPoint(coordinates[0]);
+    vtkIdType lineId[2];
+    lineId[0] = points->InsertNextPoint(start);
+
+    for(int i = 1; i < LINE_COUNT; i++)
+    {
+        double *end = createPoint(coordinates[i]);
+        lineId[1] = points->InsertNextPoint(end);
+
+        vtkLine *line = vtkLine::New();
+
+        line->GetPointIds()->SetId(0, lineId[0]);
+        line->GetPointIds()->SetId(1, lineId[1]);
+
+        lines->InsertNextCell(line);
+
+        start = end;
+        lineId[0] = lineId[1];
+    }
+
+    linesPolyData->SetPoints(points);
+    linesPolyData->SetLines(lines);
+
+    return linesPolyData;
+}
 vtkPolyData *vtkGeoJSONFeature::extractMultiLineString(Json::Value coordinateArray)
 {
-    return vtkPolyData::New();
-}
+    //Extract Multiple Line strings from the geoJSON Geometry and generate the corresponding vtkPolyData
+    //Check if Coordinate Array corresponds to Multi Line String
 
+    vtkAppendPolyData *appendPolyDataFilter = vtkAppendPolyData::New();
+    int LINE_STRING_COUNT = coordinateArray.size();
+
+    for(int i = 0; i < LINE_STRING_COUNT; i++)
+    {
+        vtkPolyData *lineStringPolyData = extractLineString(coordinateArray[i]);
+        appendPolyDataFilter->AddInputData(lineStringPolyData);
+    }
+
+    vtkCleanPolyData *cleanPolyData = vtkCleanPolyData::New();
+    cleanPolyData->SetInputConnection(appendPolyDataFilter->GetOutputPort());
+    cleanPolyData->Update();
+
+    return cleanPolyData->GetOutput();
+}
 vtkPolyData *vtkGeoJSONFeature::extractPolygon(Json::Value coordinate)
 {
-    return vtkPolyData::New();
-}
+    //Extract the polygon from the geoJSON feature and generate the corresponding vtkPolyData
+    //Check if Coordinate Array corresponds to Polygon
 
+    bool POLYGON_WITH_HOLES = coordinate.size() > 1 ? true : false;
+
+    vtkPolyData *exteriorPolygonPolyData = vtkPolyData::New();
+    vtkPoints *points = vtkPoints::New();
+    vtkPolygon *exteriorPoly = vtkPolygon::New();
+    vtkCellArray *polys = vtkCellArray::New();
+
+    //For exterior Polygon
+    Json::Value exteriorPolygon = coordinate[0];
+
+    int EXTERIOR_POLYGON_VERTEX_COUNT = exteriorPolygon.size() - 1;
+    exteriorPoly->GetPointIds()->SetNumberOfIds(EXTERIOR_POLYGON_VERTEX_COUNT);
+
+    for(int i = 0; i < EXTERIOR_POLYGON_VERTEX_COUNT; i++)
+    {
+        double *point = createPoint(exteriorPolygon[i]);
+        vtkIdType id = points->InsertNextPoint(point);
+        exteriorPoly->GetPointIds()->SetId(i, id);
+    }
+    polys->InsertNextCell(exteriorPoly);
+
+    exteriorPolygonPolyData->SetPoints(points);
+    exteriorPolygonPolyData->SetPolys(polys);
+
+    if(!POLYGON_WITH_HOLES)
+        return exteriorPolygonPolyData;
+
+    //Modify polydata to support polygon with holes
+    //Probably use delaunay triangulation to create polygon with holes
+
+    return exteriorPolygonPolyData;
+}
 vtkPolyData *vtkGeoJSONFeature::extractMultiPolygon(Json::Value coordinateArray)
 {
-    return vtkPolyData::New();
-}
+    //Extract the Multiple polygons from the geoJSON geometry and generate the vtkPolyData
+    //Check if Coordinate Array corresponds to Multi Polygon
 
+    vtkAppendPolyData *appendPolyDataFilter = vtkAppendPolyData::New();
+
+    int POLYGON_COUNT = coordinateArray.size();
+
+    for(int i = 0; i < POLYGON_COUNT; i++)
+    {
+        vtkPolyData *polygonPolyData = extractPolygon(coordinateArray[i]);
+        appendPolyDataFilter->AddInputData(polygonPolyData);
+    }
+
+    vtkCleanPolyData *cleanPolyData = vtkCleanPolyData::New();
+    cleanPolyData->SetInputConnection(appendPolyDataFilter->GetOutputPort());
+    cleanPolyData->Update();
+
+    return cleanPolyData->GetOutput();
+}
 vtkPolyData *vtkGeoJSONFeature::extractGeoJSONFeature(Json::Value root)
 {
+    //Extract the geometry and properties of a geoJSON feature and return the correspongsing vtkPolyData
+
+    this->featureRoot = root;
+
     Json::Value type = root.get("type", -1);
     Json::Value geometry = root.get("geometry", -1);
     Json::Value properties = root.get("properties", -1);
@@ -119,9 +226,10 @@ vtkPolyData *vtkGeoJSONFeature::extractGeoJSONFeature(Json::Value root)
     */
     return geometryPolyData;
 }
-
 vtkPolyData *vtkGeoJSONFeature::extractGeoJSONFeatureGeometry(Json::Value root)
 {
+    //Extract Geometry for a geoJSON Feature
+
     Json::Value type = root.get("type", -1);
     Json::Value coordinates = root.get("coordinates", -1);
     outputData = vtkPolyData::New();
@@ -173,14 +281,17 @@ vtkPolyData *vtkGeoJSONFeature::extractGeoJSONFeatureGeometry(Json::Value root)
     }
 
 }
+
 bool vtkGeoJSONFeature::isEqual(vtkStdString str1, vtkStdString str2)
 {
+    //Case Insensitive string comparison
     if(str1.length() != str2.length())
         return false;
 
     int len = str1.length();
     for(int i = 0; i < len; i++)
     {
+        //Exact Match of characters or Capital - Small match or Small Capital match of alphabets
         if(str1[i] != str2[i] && str1[i] - 32 != str2[i] && str1[i] != str2[i] - 32)
             return false;
     }
--- vtkgeojsonfeature.h
+++ vtkgeojsonfeature.h
@@ -4,6 +4,8 @@
 #include <vtkPolyData.h>
 #include <vtkPoints.h>
 #include <vtkCellArray.h>
+#include <vtkAppendPolyData.h>
+#include <vtkCleanPolyData.h>
 #include <vtkLine.h>
 #include <vtkPolygon.h>
 #include <vtk_jsoncpp.h>
@@ -20,22 +22,45 @@ class vtkGeoJSONFeature
 {
 public:
     vtkGeoJSONFeature();
+
+    //Extract the geometry and properties corresponding to the geoJSON feature stored at root
     vtkPolyData *extractGeoJSONFeature(Json::Value root);
+
+    //Return the vtkPolyData corresponding to the geoJSON feature stord in featureRoot
     vtkPolyData *GetOutput();
 
 protected:
+    //vtkPolyData containing the polydata generated from the geoJSON feature
     vtkPolyData *outputData;
 
+    //Json::Value featureRoot corresponds to the root of the geoJSON feature from which the geometry and properties are to be extracted
+    Json::Value featureRoot;
+
+    //Extract geoJSON geometry into vtkPolyData *
     vtkPolyData *extractGeoJSONFeatureGeometry(Json::Value root);
+
+    //Extract geoJSON Point into vtkPolyData *
     vtkPolyData *extractPoint(Json::Value coordinates);
+
+    //Extract geoJSON MultiPoint into vtkPolyData *
     vtkPolyData *extractMultiPoint(Json::Value coordinates);
+
+    //Extract geoJSON Line String into vtkPolyData *
     vtkPolyData *extractLineString(Json::Value coordinates);
+
+    //Extract geoJSON Multi Line String into vtkPolyData *
     vtkPolyData *extractMultiLineString(Json::Value coordinateArray);
+
+    //Extract geoJSON Polygon into vtkPolyData *
     vtkPolyData *extractPolygon(Json::Value coordinate);
+
+    //Extract geoJSON Multi Polygon into vtkPolyData *
     vtkPolyData *extractMultiPolygon(Json::Value coordinateArray);
 
+    //Point[] from its JSON equivalent
     double *createPoint(Json::Value coordinates);
 
+    //Case insensitive string comparison
     bool isEqual(vtkStdString str1, vtkStdString str2);
 };
 
