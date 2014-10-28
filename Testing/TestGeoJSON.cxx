3e3e141a4e184d8b46e52ecbf0b51dcd12511231
John Tourtellott <john.tourtellott@kitware.com>
2014-10-28 11:20:08 -0400

    Generalize GeoJSON test and include json-schema input for properties
    Add "ny points" test files (geometry & schema)

--- Testing/TestGeoJSON.cxx
+++ Testing/TestGeoJSON.cxx
@@ -5,42 +5,69 @@
 #include <vtkRenderWindow.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkPolyDataWriter.h>
+#include <vtksys/CommandLineArguments.hxx>
 
 //Reader includes
 #include "vtkGeoJSONReader.h"
 
 #include <fstream>
 #include <iostream>
+#include <string>
 
+bool SetupFeatureProperties(Json::Value& schemaNode, vtkGeoJSONReader *reader);
 int TestGeoJSON(int argc, char **argv);
 
+//----------------------------------------------------------------------------
 int main(int argc, char **argv)
 {
    return TestGeoJSON(argc, argv);
 }
 
+//----------------------------------------------------------------------------
 int TestGeoJSON(int argc, char **argv)
 {
-  if (argc < 2)
+  // Setup command line arguments
+  bool showHelp = false;
+  std::string inputGeoJSONFile;
+  bool stringInputMode = false;
+  std::string inputSchemaFile;
+  std::string outputVtkFile;
+
+  vtksys::CommandLineArguments arg;
+  arg.Initialize(argc, argv);
+  arg.StoreUnusedArguments(true);
+  arg.AddArgument("-h", vtksys::CommandLineArguments::NO_ARGUMENT,
+                  &showHelp, "show help message");
+  arg.AddArgument("--help", vtksys::CommandLineArguments::NO_ARGUMENT,
+                  &showHelp, "show help message");
+  arg.AddArgument("-s", vtksys::CommandLineArguments::NO_ARGUMENT,
+                  &stringInputMode, "flag to load file as string before parsing");
+  arg.AddArgument("-j", vtksys::CommandLineArguments::SPACE_ARGUMENT,
+                  &inputSchemaFile, "json-schema file specifying feature properties");
+  arg.AddArgument("-v", vtksys::CommandLineArguments::SPACE_ARGUMENT,
+                  &outputVtkFile, "VTK output file");
+
+  if (argc < 2 || !arg.Parse() || showHelp)
     {
-    std::cout << "\n"
+    std::cerr << "\n"
               << "Test vtkGeoJSONReader" << "\n"
-              << "Usage  TestGeoJSON  input_file.json  [StringInputMode=0]"
-              <<"  [output.vtk]"
+              << "Usage:  TestGeoJSON  input-geojson-file  [options]" << "\n"
+              << "Options:" << "\n"
+              << arg.GetHelp()
               << std::endl;
     return -1;
     }
 
-  //Reader
-  vtkGeoJSONReader *reader = vtkGeoJSONReader::New();
+  inputGeoJSONFile = argv[1];
 
-  bool useStringInputMode = (argc > 2) && (argv[2] != "0");
-  if (useStringInputMode)
+  // Instantiate the GeoJSON reader
+  vtkGeoJSONReader *reader = vtkGeoJSONReader::New();
+  if (stringInputMode)
     {
     std::cout << "Setting StringInputMode true" << std::endl;
     reader->StringInputModeOn();
     // Load source file
-    std::ifstream in(argv[1], std::ios::in | std::ios::binary);
+    std::ifstream in(inputGeoJSONFile.c_str(), std::ios::in | std::ios::binary);
     if (in)
       {
       std::string content(std::istreambuf_iterator<char>(in.rdbuf()),
@@ -57,25 +84,58 @@ int TestGeoJSON(int argc, char **argv)
   else
     {
     // Set source file
-    reader->SetFileName(argv[1]);
+    reader->SetFileName(inputGeoJSONFile.c_str());
     }
 
-  //Read the output
-  reader->Update();
+  // Process schema
+  if (!inputSchemaFile.empty())
+    {
+    // Open the file
+    ifstream file;
+    file.open(inputSchemaFile.c_str());
 
+    if (!file.is_open())
+      {
+      std::cerr << "Unable to open file " << inputSchemaFile.c_str()
+      << std::endl;
+      return VTK_ERROR;
+      }
+
+    // Parse the file contents into Json::Value
+    Json::Value schemaRoot;
+    Json::Reader schemaReader;
+    bool parsedSuccess = schemaReader.parse(file, schemaRoot, false);
+    file.close();
+    if (!parsedSuccess)
+      {
+      std::cerr << "Failed to parse JSON\n"
+                << schemaReader.getFormatedErrorMessages() << std::endl;
+      return VTK_ERROR;
+      }
+    if (!SetupFeatureProperties(schemaRoot, reader))
+      {
+      std::cerr << "Problem parsing schema input file" << std::endl;
+      return VTK_ERROR;
+      }
+    }
+
+
+  // Read the input
+  reader->Update();
   vtkPolyData *outputData = reader->GetOutput();
 
-  if (argc > 2)
+  // Write vtk file output
+  if (!outputVtkFile.empty())
     {
-    std::cout << "Writing poly data to " << argv[2] << std::endl;
+    std::cout << "Writing poly data to " << outputVtkFile << std::endl;
     vtkSmartPointer<vtkPolyDataWriter> writer =
       vtkSmartPointer<vtkPolyDataWriter>::New();
-    writer->SetFileName(argv[2]);
+    writer->SetFileName(outputVtkFile.c_str());
     writer->SetInputData(outputData);
     writer->Update();
     }
 
-  //Visualise in a render window
+  // Visualise in a render window
   vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   mapper->SetInputData(outputData);
 
@@ -94,4 +154,104 @@ int TestGeoJSON(int argc, char **argv)
   renderWindowInteractor->Start();
 
   return EXIT_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+bool SetupFeatureProperties(Json::Value& schemaNode, vtkGeoJSONReader *reader)
+{
+  if (schemaNode.isNull())
+    {
+    return false;
+    }
+  if (!schemaNode.isObject())
+    {
+    std::cerr << "properties-schema node is not an object" << std::endl;;
+    return false;
+    }
+
+  // Get properties node
+  Json::Value propertiesNode = schemaNode["properties"];
+  if (!propertiesNode.isObject())
+    {
+    std::cerr << "schema properties node either missing or wrong json type"
+              << std::endl;;
+    return false;
+    }
+
+  Json::Value::Members propertyNames = propertiesNode.getMemberNames();
+  Json::Value::Members::const_iterator nameIter = propertyNames.begin();
+  for (; nameIter != propertyNames.end(); nameIter++)
+    {
+    std::string name = *nameIter;
+    //std::cout << "Property name " << name << std::endl;
+
+    Json::Value propertyNode = propertiesNode[name];
+    Json::Value typeNode = propertyNode["type"];
+    if (typeNode.isNull() || !typeNode.isString())
+      {
+      std::cerr << "Invalid schema for property " << name
+                << " -- no \"type\" specified" << std::endl;;
+      continue;
+      }
+    vtkIdType vtkDataType = VTK_VOID;
+    std::string typeString = typeNode.asString();
+    if ("boolean" == typeString)
+      {
+      vtkDataType = VTK_BIT;
+      }
+    else if ("integer" == typeString)
+      {
+      vtkDataType = VTK_INT;
+      }
+    else if ("number" == typeString)
+      {
+      vtkDataType = VTK_DOUBLE;
+      }
+    else if ("string" == typeString)
+      {
+      vtkDataType = VTK_STRING;
+      }
+
+    if (VTK_VOID == vtkDataType)
+      {
+      std::cerr << "Unspported data type " << typeString.c_str()
+                << " for property " << name.c_str() << std::endl;
+      }
+
+    vtkVariant defaultValue;
+    Json::Value defaultNode = propertyNode["default"];
+    switch (defaultNode.type())
+      {
+      case Json::nullValue:
+        std::cerr << "no default value specified for property "
+                  << name.c_str() << std::endl;
+        break;
+
+      case Json::intValue:
+      case Json::uintValue:
+        defaultValue = vtkVariant(defaultNode.asInt());
+        break;
+
+      case Json::realValue:
+        defaultValue = vtkVariant(defaultNode.asDouble());
+        break;
+
+      case Json::stringValue:
+        defaultValue = vtkVariant(defaultNode.asString());
+        break;
+
+      default:
+        std::cerr << "Unsupported default node type: "
+                  << defaultNode.type() << std::endl;
+        break;
+      }
+
+    std::size_t length = name.size();
+    char charName[length+1];
+    name.copy(charName, length);
+    charName[length] = '\0';
+    reader->AddFeatureProperty(charName, vtkDataType, defaultValue);
+    }
+
+  return true;
 }
